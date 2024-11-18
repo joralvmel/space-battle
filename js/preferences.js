ae3e211d48e637a68776f93b539d41b09227fec9
joralvmel <joralvmel@gmail.com>
2024-11-18 16:18:27 +0100

    add preferences modal

--- js/preferences.js
+++ js/preferences.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     loadPreferences();
+    setupModalListeners();
 });
 
 function savePreferences() {
@@ -7,7 +8,9 @@ function savePreferences() {
     const gameTime = document.getElementById('gameTime').value;
     localStorage.setItem('numUFOs', numUFOs);
     localStorage.setItem('gameTime', gameTime);
-    alert('Preferences saved');
+
+    // Show the modal
+    $('#preferencesModal').modal('show');
 }
 
 function loadPreferences() {
@@ -15,4 +18,14 @@ function loadPreferences() {
         document.getElementById('numUFOs').value = localStorage.getItem('numUFOs') || 5;
         document.getElementById('gameTime').value = localStorage.getItem('gameTime') || 60;
     }
+}
+
+function setupModalListeners() {
+    document.getElementById('play-game').addEventListener('click', () => {
+        window.location.href = 'play.html';
+    });
+
+    document.getElementById('close-modal').addEventListener('click', () => {
+        $('#preferencesModal').modal('hide');
+    });
 }
